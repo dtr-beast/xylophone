a7e4c8d32d21607ab12b511c323a7c7e469d337c
dtr-beast <55450440+dtr-beast@users.noreply.github.com>
2021-01-08 16:37:32 +0530

    Added Assets and Made a stub testing app for playing Audio

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,64 @@ void main() {
   runApp(MyApp());
 }
 
+final player = AudioCache();
+
+// An Expanded Flat Play Button for Notes
+Expanded expandedFlatPlayButton(Color backGroundColor, int noteNum) {
+  return Expanded(
+    child: FlatButton(
+      padding: EdgeInsets.all(0.0),
+      color: backGroundColor,
+      onPressed: () {
+        player.play('note$noteNum.wav');
+      }, child: null,
+    ),
+  );
+}
+
 class MyApp extends StatelessWidget {
-  final player = AudioCache();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                player.play('note1.wav');
-              },
-              child: Text('Click Me!'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              expandedFlatPlayButton(Colors.red, 1),
+              expandedFlatPlayButton(Colors.orange, 2),
+              expandedFlatPlayButton(Colors.yellow, 3),
+              expandedFlatPlayButton(Colors.green, 4),
+              expandedFlatPlayButton(Colors.teal, 5),
+              expandedFlatPlayButton(Colors.blue, 6),
+              expandedFlatPlayButton(Colors.purple, 7),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+// Stub FlatPlayButton Class
+// class FlatPlayButton {
+//   Color backGroundColor;
+//   String note;
+//   String text;
+//   FlatPlayButton(Colors color, String note, String text)
+//   {
+//     this.backGroundColor = color as Color;
+//     this.note = note;
+//     this.text = text;
+//   }
+//   Widget build(BuildContext context) {
+//   return FlatButton(
+//     color: this.backGroundColor,
+//     onPressed: () {
+//       player.play(this.note);
+//     },
+//     child: Text(this.text),
+//   );
+//   }
+// }
